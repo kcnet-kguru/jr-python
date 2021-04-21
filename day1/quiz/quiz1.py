9def7c3e36a45b83cc399fe99ed4a218d8e7dd04
rkd0812 <rkd0812@gmail.com>
2021-04-21 20:28:18 +0900

    Day1 퀴즈

---
+++ day1/quiz/quiz1.py
@@ -0,0 +1,15 @@
+# 주어진 숫자를 10으로 나누어 나머지가 0인 가장 가까운 숫자를 반환합니다.
+#
+# Example input:
+#
+# 22
+# 25
+# 37
+# Expected output:
+#
+# 20
+# 30
+# 40
+
+def closest_multiple_10(i):
+    return i
---
+++ day1/quiz/quiz2.py
@@ -0,0 +1,10 @@
+# 당신은 마법의 우물에 서 있습니다. 두 개의 양의 정수가 기록되어 있습니다
+# a와 b. 마법 구슬을 우물에 던질 때마다 a * b 달러를주고 a와 b 모두 1 씩 증가합니다.
+# 마법 구슬이 n 개 있습니다. 당신은 얼마나 많은 돈을 벌 것입니까?
+
+# Example
+# For a = 1, b = 2 and n = 2, the output should be 8
+
+
+def magical_well(a, b, n):
+    return 0
