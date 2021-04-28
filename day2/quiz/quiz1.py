08d638fdb46b3f7c5b617be629848f1a578d581c
kcnet-kguru <jsi@kcnet.co.kr>
2021-04-28 18:02:47 +0900

    Day2 퀴즈

---
+++ day2/quiz/quiz1.py
@@ -0,0 +1,16 @@
+# 정수 배열/목록 []이(가) 주어진 경우, k maximum 숫자의 곱을 찾습니다.
+# ex)
+# max_product ([4, 3, 5], 2) ==>  return (20)
+# 5 * 4 = 20
+#
+# max_product ([8, 10 , 9, 7], 3) ==>  return (720)
+# 8 * 9 * 10 = 720
+#
+# max_product([10, 8, 3, 2, 1, 4, 10], 5) ==> return (9600)
+# 10 * 10 * 8 * 4 * 3 = 9600
+#
+# max_product ([10, 3, -1, -27] , 3)  return (-30)
+# 10 * 3 * -1 = -30
+
+def max_product(obj, k):
+    pass
