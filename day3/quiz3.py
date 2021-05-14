4d6cbbffc55b2b66f3b9092b8daaff3b7717f8bc
baesang0522 <37433827+baesang0522@users.noreply.github.com>
2021-05-14 23:15:44 +0900

    Add files via upload

---
+++ day3/quiz3.py
@@ -0,0 +1,10 @@
+# 정수의 배열 Arr[] 이 주어졌을 때 prod[i]는 Arr[i]를 제외한 나머지 요소들의 곱을 가지고 있는 같은 크기의 배열을 반환합니다.
+# Example
+# productArray ([12,20]) ==>  return {20,12}
+# productArray ([1,5,2]) ==> return {10,2,5}
+# productArray ([10,3,5,6,2]) return ==> {180,600,360,300,900}
+from functools import reduce
+
+
+def productArray(arr):
+    return [int(reduce(lambda x,y:x*y, arr)/arr[idx]) for idx in range(len(arr))]
